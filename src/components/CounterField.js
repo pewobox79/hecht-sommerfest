74e44a6e45c4b8dd969c54365f495d3d9e3d9d07
Peter Wolf <pewobox@Peters-Air.fritz.box>
2021-05-20 00:03:27 +0200

    update favicon and mobile

--- src/components/CounterField.js
+++ src/components/CounterField.js
@@ -1,14 +1,7 @@
 import React from "react";
-import {Date, Time} from "../styled";
-import styled from "styled-components";
 import DateField from "./DateField";
 import TimeField from "./TimeField";
-
-const CounterFieldStyle = styled.div`
-display: flex;
-  justify-content: center;
-  background-color: green;
-  `
+import {CounterFieldStyle} from "../styled";
 
 
 function CounterField() {
