c2f5828d3fdfc608495e3df8414ed11d872be1ba
Peter Wolf <pewobox@Peters-Air.fritz.box>
2021-06-17 10:38:13 +0200

    email function included with elasticmail

--- src/components/Anmeldeformular.js
+++ src/components/Anmeldeformular.js
@@ -6,51 +6,89 @@ import {
     AnmeldeInputBegleitung,
     AnmeldeLabel,
     AnmeldeTitel,
-    CheckboxLabel, OuterLeft,
+    CheckboxLabel,
     TimeStyle,
 } from "../styled";
 
 function Anmeldeformular() {
 
-    const [meldungAllein, setMeldungAllein] = useState()
-    const [meldungBegleitung, setMeldungBegleitung] = useState()
-    const [absage, setAbsage] = useState()
+    const [meldungAllein, setMeldungAllein] = useState("werde mit")
+    const [meldungBegleitung, setMeldungBegleitung] = useState("ohne Begleitung kommen")
+    const [absage, setAbsage] = useState("kann kommen")
     const [teilnehmerName, setTeilnehmerName] = useState()
-    const [begleitungName, setBegleitungName] = useState()
-    const [shuttle, setShuttle] = useState()
+    const [begleitungName, setBegleitungName] = useState("")
+    const [shuttle, setShuttle] = useState(" Ich benötige keinen Shuttleservice")
     const [bemerkung, setBemerkung] = useState()
-
 
     const [mitBegleitung, setMitBegleitung] = useState(false)
 
-    function handleAnmeldung(e) {
+    /* SmtpJS.com - v3.0.0 */
+    const Email = {
+        send: function (a) {
+            return new Promise(function (n, e) {
+                // eslint-disable-next-line no-unused-expressions,no-sequences
+                a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
+                let t = JSON.stringify(a);
+                Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+                    n(e)
+                })
+            })
+        }, ajaxPost: function (e, n, t) {
+            let a = Email.createCORSRequest("POST", e);
+            // eslint-disable-next-line no-unused-expressions
+            a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () {
+                let e = a.responseText;
+                null != t && t(e)
+            }, a.send(n)
+        }, ajax: function (e, n) {
+            let t = Email.createCORSRequest("GET", e);
+            // eslint-disable-next-line no-unused-expressions
+            t.onload = function () {
+                let e = t.responseText;
+                null != n && n(e)
+            }, t.send()
+        }, createCORSRequest: function (e, n) {
+            let t = new XMLHttpRequest;
+            // eslint-disable-next-line no-undef
+            return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t
+        }
+    };
+
+    function sendMail(e) {
         e.preventDefault()
-        alert(absage + meldungAllein + "Name" + teilnehmerName + " " + "Begleitung: " + begleitungName + shuttle + " " + "Bemerkung" + bemerkung)
-
+        Email.send({
+            SecureToken: "bc61b703-5e46-49b7-ad19-be1847cf6673",
+            To: 'y.wolf@hecht.eu',
+            Cc: 'julien.lobo.13@gmail.com',
+            From: "pewobox79@gmail.com",
+            Subject: `Sommerfest Rückmeldung`,
+            Body: `Hallo - ich ${teilnehmerName} ${meldungAllein} ${meldungBegleitung} ${begleitungName}. ${shuttle}. Sonstige Info: ${bemerkung}`
+        }).then(
+            message => alert("Ihre Rückmeldung zum Sommerfest wurde an Yessica Wolf verschickt.")
+        );
     }
-
 
     return (
         <>
 
             <AnmeldeTitel>Wir bitten um Rückmeldung bis zum <TimeStyle>04.07.2021</TimeStyle></AnmeldeTitel>
-            <Anmeldeform enctype="text/plain">
+            <Anmeldeform onSubmit={sendMail} method="post" enctype="text/plain">
                 <AnmeldeLabel>Anmeldung</AnmeldeLabel>
 
                 <AnmeldeCheckbox type="checkbox" id="ja1" name="allein" value={meldungAllein}
-                                 onChange={e => setMeldungAllein(e = ("ich komme allein"))}/>
-                <CheckboxLabel for="allein">Ja, ich komme allein zum Event</CheckboxLabel><br/>
+                                 onChange={e => setMeldungAllein(e = ("komme allein"))}/>
+                <CheckboxLabel htmlFor="allein">Ja, ich komme allein zum Event</CheckboxLabel><br/>
                 <AnmeldeCheckbox type="checkbox" id="ja2" name="begleitperson"
                                  value={meldungBegleitung}
-                                 onChange={e => setMeldungBegleitung(e = ("ich komme mit begleitung"))}
+                                 onChange={e => setMeldungBegleitung(e = ("mit begleitung kommen"))}
                                  onClick={e => setMitBegleitung(e.target.checked)}/>
-                <CheckboxLabel for="begleitperson">Ja, ich komme mit einer Begleitperson</CheckboxLabel><br/>
+                <CheckboxLabel htmlFor="begleitperson">Ja, ich komme mit einer Begleitperson</CheckboxLabel><br/>
                 <AnmeldeCheckbox type="checkbox" id="shuttleservice" name="shuttle"
-                                 value={shuttle} onChange={e => setShuttle(e = "Ich benötige einen Shuttle Service")}/>
-                <CheckboxLabel for="shuttle">Ich habe interesse am Shuttle Service</CheckboxLabel><br/>
+                                 value={shuttle} onChange={e => setShuttle(e = " Ich benötige einen Shuttle Service")}/>
+                <CheckboxLabel htmlFor="shuttle">Ich habe interesse am Shuttle Service</CheckboxLabel><br/>
                 <AnmeldeCheckbox type="checkbox" id="nein" name={absage}
                                  value={absage} onChange={e => setAbsage(e = "Nein, ich kann leider nicht kommen!")}/>
-                <CheckboxLabel for="ich komme">Nein, ich kann leider nicht kommen</CheckboxLabel><br/>
+                <CheckboxLabel htmlFor="ich komme">Nein, ich kann leider nicht kommen</CheckboxLabel><br/>
 
                 <AnmeldeInput type="text" value={teilnehmerName} onChange={e => (setTeilnehmerName(e.target.value))}
                               placeholder="Vor- und Zuname" required/>
@@ -62,7 +100,7 @@ function Anmeldeformular() {
                 <AnmeldeBemerkung type="text" value={bemerkung} onChange={e => (setBemerkung(e.target.value))}
                                   placeholder="Bemerkung"/>
 
-                <AnmeldeButton onClick={handleAnmeldung}>Abschicken</AnmeldeButton>
+                <AnmeldeButton>Abschicken</AnmeldeButton>
             </Anmeldeform>
 
         </>
